5eb1a359ccf0c1c8f204d8203ce6af4ff2b633a7
bakajstep <stepan.bakaj@gmail.com>
2022-10-10 18:25:37 +0200

    Initial commit

---
+++ src/test/java/study/fit/wis/WisApplicationTests.java
@@ -0,0 +1,13 @@
+package study.fit.wis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
