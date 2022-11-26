a4804085e77a7abd8acbd529b43bd90e12602a60
IncredibleJoe <dusancicmis2@gmail.com>
2022-11-26 14:33:24 +0100

    term routes

--- api/models/term_model.py
+++ api/models/term_model.py
@@ -9,6 +9,30 @@ class Term(db.Model):
     from_time = db.Column(db.Time(), nullable=False)
     to_time = db.Column(db.Time(), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id', ondelete="CASCADE"))
-    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id', ondelete="CASCADE"))
     rank = db.relationship('Rank', backref='Term')
     term_date = db.relationship('TermDate', backref='Term')
+
+
+    def __repr__(self):
+        return f'Term {self.id}'
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_by_id(cls, _id):
+        return cls.query.filter_by(id=_id).first()
+
+    @classmethod
+    def get_by_label(cls, _label):
+        return cls.query.filter_by(label=_label).first()
+
+    def to_dict(self):
+        cls_dict = {'id': self.id, 'label': self.label, 'rating': self.rating, 'min_points': self.min_points,
+                    'date':self.date, 'course_id': self.course_id, 'room_id': self.room_id}
+        return cls_dict
+
+    def to_json(self):
+        return self.to_dict()
