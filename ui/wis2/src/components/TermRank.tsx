1bec1597adf60a3c10ee16cf4cecae2f2d393961
adamkankovsky <adamkankovsky@gmail.com>
2022-12-13 23:04:14 +0100

    bug fix

--- ui/wis2/src/components/TermRank.tsx
+++ ui/wis2/src/components/TermRank.tsx
@@ -7,7 +7,9 @@ import {
     DialogContent,
     DialogContentText,
     DialogTitle,
-    MenuItem, Select, SelectChangeEvent
+    MenuItem,
+    Select,
+    SelectChangeEvent
 } from "@mui/material";
 import {DataGrid, GridColDef,} from "@mui/x-data-grid";
 import * as React from "react";
